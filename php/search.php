4bb4982fbb6b73f94eb0d572b2a693a4014686f9
sneha-meto <snehameto@gmail.com>
2021-01-07 14:26:04 +0530

    search and quicklinks

---
+++ php/search.php
@@ -0,0 +1,25 @@
+<?php
+
+$conn = mysqli_connect("localhost", "root", '', "vendakka");  
+
+if(mysqli_connect_errno()) {  
+    die("Failed to connect with MySQL: ". mysqli_connect_error());  
+}
+
+// else if(isset($_GET['submit']))
+// {
+$search_string = $_POST['search'];
+$query="SELECT * FROM stock natural join farmer natural join users WHERE product_name LIKE '%$search_string%'";
+$res=mysqli_query($conn,$query);
+
+$rows = [];
+while ($row = mysqli_fetch_assoc($res)) {
+    $rows[] = $row;
+}
+echo json_encode($rows);
+
+
+mysqli_close($conn);
+// }
+
+?>
